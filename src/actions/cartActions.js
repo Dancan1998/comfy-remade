3347118e6fc0f9f7867875bf9c2548284da26599
Dancan Chibole <dancankingstar@gmail.com>
2021-02-27 09:34:11 -0800

    action placeholders

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -9,3 +9,9 @@ import {
 export const addToCart = (id, color, amount, product) => (dispatch) => {
   dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
 };
+
+export const removeItem = (id) => (dispatch) => {};
+
+export const toggleAmount = (id, value) => (dispatch) => {};
+
+export const clearCart = () => (dispatch) => {};
