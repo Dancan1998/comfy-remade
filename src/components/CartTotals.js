920746ea149cd1425ca71736006e9bd35e000c17
Dancan Chibole <dancankingstar@gmail.com>
2021-02-27 11:08:35 -0800

    removed cart context

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { useCartContext } from "../context/cart_context";
 import { formatPrice } from "../utils/helpers";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
