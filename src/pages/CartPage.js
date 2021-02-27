8d4f85cb9188e08f2df84a05ce474b90164aaed5
Dancan Chibole <dancankingstar@gmail.com>
2021-02-27 09:40:24 -0800

    cart page setup

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useCartContext } from '../context/cart_context'
-import { Link } from 'react-router-dom'
-import { CartContent, PageHero } from '../components'
+import React from "react";
+import styled from "styled-components";
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
+import { CartContent, PageHero } from "../components";
+import { removeItem, toggleAmount, clearCart } from "../actions/cartActions";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartPage = () => {
-  return <h4>cart page</h4>
-}
+  const dispatch = useDispatch();
+
+  const cartContext = useSelector((state) => state.cartContext);
+  const { cart } = cartContext;
+
+  if (cart.length < 1) {
+    return (
+      <Wrapper className="page-100">
+        <div className="empty">
+          <h2>Your cart is empty</h2>
+          <Link className="btn" to="/products">
+            shop now
+          </Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <main>
+      <PageHero title="cart" />
+      <Wrapper className="page">
+        <CartContent />
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.main`
   .empty {
@@ -16,6 +43,6 @@ const Wrapper = styled.main`
       text-transform: none;
     }
   }
-`
+`;
 
-export default CartPage
+export default CartPage;
